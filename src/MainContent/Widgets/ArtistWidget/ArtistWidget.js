b91efb81967731208ce1d52d2bb90e584ecfa2c3
FrankM-7 <frankm-7@tamu.edu>
2024-05-18 11:02:39 -0500

    finished frontend

--- src/MainContent/Widgets/ArtistWidget/ArtistWidget.js
+++ src/MainContent/Widgets/ArtistWidget/ArtistWidget.js
@@ -1,21 +1,37 @@
 import "./ArtistWidget.css";
 import { Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import travis from '../../../images/travis.webp';
+import { Text } from '../LatestReleaseWidget/LatestReleaseWidget';
+import play from '../../../images/play-button.png';
+import shuffle from '../../../images/shuffle.png';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#ffffff',
-    ...theme.typography.body2,
-    textAlign: 'center',
-    boxShadow: 'none',
-    width: '250px',
-  }));
-
-  
 function ArtistWidget({style}) {
     return (
-        <Item style={style}>
-            <h1>Travis Scott</h1>
-        </Item>
+        <div className="artist-containers">
+            <div className="a-row">
+                <div className="artist-img">
+                    <img src={travis} alt="travis" />
+                </div>
+            </div>
+            <div className="a-row">
+                <div className="a-top">
+                    <Text className="verified-artist">Verified Artist</Text>
+                    <Text className="artist-name" style={{fontSize: '32px'}}>Travis Scott</Text>
+                    <Text className="artist-listeners">67,245,743 monthly listeners</Text>
+                </div>
+
+                <div className="a-bottom">
+                    <Text id="following" className="a-button">Following</Text>
+                    <Text id="shuffle" className="a-button">             
+                         <img className="shuffleBtn" src={shuffle} alt="shuffle" />
+                    </Text>
+                    <div className="f-play">
+                        <img src={play} alt="play"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
